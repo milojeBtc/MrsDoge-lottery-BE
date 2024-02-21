5f94f3ef66737b0c32195c566550fe556b8fa727
milojeBtc <nikolic.miloje0507@gmail.com>
2024-02-21 17:28:18 +0100

    update

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ const largeList = {};
 const smallList = {};
 const commonList = {};
 
+// Last ticket Holder
+let lastTicketAddress = '';
+let sortedUserList = [];
+
+let totalPotPrice = 0;
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Cybord" });
@@ -83,7 +89,7 @@ app.post("/api/buyticket", async (req, res) => {
   const date = new Date();
 
   console.log("/api/buyticket ==> ", req.body);
-  console.log("first api/buyticket ==> ", userList);
+  // console.log("first api/buyticket ==> ", userList);
 
   if(userList[address] > 0) userList[address] += ticketCount
   else userList[address] = ticketCount
@@ -101,10 +107,16 @@ app.post("/api/buyticket", async (req, res) => {
     commonList[address] = 1;
   }
 
+  totalPotPrice += btc;
+  lastTicketAddress = address;
+
   console.log('hugeList ==> ', hugeList);
   console.log('largeList ==> ', largeList);
   console.log('smallList ==> ', smallList);
   console.log('commonList ==> ', commonList);
+
+  console.log('totalPotPrice ==> ', totalPotPrice);
+  console.log('userList ==> ', userList);
 
   res.send(userList);
 })
@@ -135,6 +147,94 @@ app.post("/api/withdrawTicket", async (req, res) => {
 
 app.get("/api/getOwnTicketList", async (req, res) => {
   res.send(userList);
+})
+
+app.get('/api/getRarityList', async (req, res) => {
+  let payload = {
+    hugeList,
+    largeList,
+    smallList,
+    commonList
+  }
+
+  res.send(payload);
+})
+
+app.post('/api/rewardResult', async (req, res) => {
+  
+  const ended = req.body.ended;
+
+  const randomProperty = (obj) => {
+    var keys = Object.keys(obj);
+    console.log('keys in randomProperty ==> ', keys);
+    
+    keys.sort(function(){return 0.5 - Math.random()});
+    console.log('random Arr ==> ', keys);
+    return keys;
+}; 
+
+  const RarityWinnerList = {};
+  if(ended){
+    // Rarity
+    // // Huge Rarity
+    const hugeListLength = Object.keys(hugeList).length;
+    if(hugeListLength > 2){
+      RarityWinnerList['huge'] = [];
+      const randomArr = randomProperty(hugeList);
+      RarityWinnerList['huge'].push(randomArr[0]);
+      RarityWinnerList['huge'].push(randomArr[1]);
+    } else if(hugeListLength > 0 && hugeListLength < 3){
+      RarityWinnerList['huge'] = Object.keys(hugeList);
+    } else {
+      RarityWinnerList['huge'] = [];
+    }
+
+     // // Large Rarity
+     const largeListLength = Object.keys(largeList).length;
+     if(largeListLength > 5){
+       RarityWinnerList['large'] = [];
+       const randomArr = randomProperty(largeList);
+       RarityWinnerList['large'].push(randomArr[0]);
+       RarityWinnerList['large'].push(randomArr[1]);
+     } else if(largeListLength > 0 && largeListLength < 6){
+       RarityWinnerList['large'] = Object.keys(largeList);
+     } else {
+       RarityWinnerList['large'] = [];
+     }
+
+     // // Small Rarity
+     const smallListLength = Object.keys(smallList).length;
+     if(smallListLength > 10){
+       RarityWinnerList['small'] = [];
+       const randomArr = randomProperty(smallList);
+       RarityWinnerList['small'].push(randomArr[0]);
+       RarityWinnerList['small'].push(randomArr[1]);
+     } else if(smallListLength > 0 && smallListLength < 11){
+       RarityWinnerList['small'] = Object.keys(smallList);
+     } else {
+       RarityWinnerList['small'] = [];
+     }
+
+    // Top Ticker Holder
+    let temp = Object.fromEntries(
+      Object.entries(userList).sort(([, a], [, b]) => b - a)
+      )
+
+    sortedUserList = [];
+
+    Object.keys(temp).map((value, index) => {
+      if(index < 3) sortedUserList.push(value);
+    })
+
+  }
+
+  console.log('RarityWinnerList ==> ', RarityWinnerList);
+  console.log('sortedUserList ==> ', sortedUserList);
+  res.send({
+    RarityWinnerList,
+    sortedUserList,
+    lastTicketAddress
+  });
 })
 
 // set port, listen for requests
